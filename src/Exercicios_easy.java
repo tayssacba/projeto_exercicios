77e79296f9b6ecfbc5ed7cd921f426173f683aa7
tayssacba <tayssa.asakawa@hotmail.com>
2024-10-15 17:58:11 -0300

    criação do projeto de exercicios

---
+++ src/Exercicios_easy.java
@@ -0,0 +1,16 @@
+public class Exercicios_easy {
+
+    public static void main(String[] args) {
+        int numerico = 0;
+        double numerosDecimais = 0.0;
+        String texto = "Teste";
+
+        if (numerico > 0) {
+            System.out.println("Numero é falso");
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("valor de I: " + i);
+        }
+    }
+}
